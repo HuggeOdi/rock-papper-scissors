5adeaa53ffcc8282ccf62c116a2498a5f1f69607
HuggeOdi <124271853+HuggeOdi@users.noreply.github.com>
2023-03-24 09:58:27 +0100

    updatering, första

---
+++ Rock-papper-scissors.py
@@ -0,0 +1,60 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+PointsC = 0
+PointsP = 0
+length = 1000
+
+
+for i in range(length):
+    #Player = int(input())
+    player = random.randint(1,3)
+    Computer = random.randint(1,3)
+    print(Computer)
+    if Player == 1:
+        if Computer == 1:
+            print("draw")
+        elif Computer == 2:
+            print("You lost")
+            PointsC += 1
+            print("Computer points" + " " + str(PointsC))
+        else:
+            print("You won")
+            PointsP += 1
+            print("PLayer points" + " " + str(PointsP))
+            
+    if Player == 2:
+        if Computer == 1:
+            print("You won")
+            PointsP += 1
+            print("PLayer points" + " " + str(PointsP))
+        elif Computer == 2:
+            print("Draw")
+        else:
+            print("You lost")
+            PointsC += 1
+            print("Computer points" + " " + str(PointsC))
+            
+            
+       
+    if Player == 3:
+        if Computer == 1:
+            print("You lost")
+            PointsC += 1
+            print("Computer points" + " " + str(PointsC))
+        elif Computer == 2:
+            print("You won")
+            PointsP += 1
+            print("Player points" + " " + str(PointsP))
+        else:
+            print("Draw")
+            
+   # plt.plot(xlist, ylist, '.', markersize = 1, label = "How many times player wins")
+   # plt.plot(xlist, ylist, '.', markersize = 1, label = "How many times computer wins")
+   # plt.xlabel("X")
+   # plt.ylabel("Y")
+   # plt.legend()
+            
+
+
+    
