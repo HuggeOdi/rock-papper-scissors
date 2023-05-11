a0a57c573125ca3da698ce368e24280a604b0cf2
HuggeOdi <124271853+HuggeOdi@users.noreply.github.com>
2023-05-11 15:37:34 +0200

    lagt till graf. Så det går att se om randint verkligen är random

--- Rock-papper-scissors.py
+++ Rock-papper-scissors.py
@@ -3,12 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 PointsC = 0
 PointsP = 0
-length = 1000
+DrawPoints = 0
+length = 10
+ylist = [0]
 
 
 for i in range(length):
     #Player = int(input())
-    player = random.randint(1,3)
+    Player = random.randint(1,3)
     Computer = random.randint(1,3)
     print(Computer)
     if Player == 1:
@@ -17,22 +19,26 @@ for i in range(length):
         elif Computer == 2:
             print("You lost")
             PointsC += 1
+            ylist.append(ylist[-1] - 1) 
             print("Computer points" + " " + str(PointsC))
         else:
             print("You won")
             PointsP += 1
+            ylist.append(ylist[-1] + 1)
             print("PLayer points" + " " + str(PointsP))
             
     if Player == 2:
         if Computer == 1:
             print("You won")
             PointsP += 1
+            ylist.append(ylist[-1] + 1)
             print("PLayer points" + " " + str(PointsP))
         elif Computer == 2:
             print("Draw")
         else:
             print("You lost")
             PointsC += 1
+            ylist.append(ylist[-1] - 1)
             print("Computer points" + " " + str(PointsC))
             
             
@@ -41,16 +47,27 @@ for i in range(length):
         if Computer == 1:
             print("You lost")
             PointsC += 1
+            ylist.append(ylist[-1] - 1)
             print("Computer points" + " " + str(PointsC))
         elif Computer == 2:
             print("You won")
+            ylist.append(ylist[-1] + 1)
             PointsP += 1
             print("Player points" + " " + str(PointsP))
         else:
             print("Draw")
             
-plt.plot(range(len(ylist)), ylist, '.', markersize = 1, label = "How many times player wins")
-   
+
+            
+    
+            
+plt.plot(range(len(ylist)), ylist, '-', markersize = 1, label = "How many times player wins")
+   # plt.xlabel("X")
+   # plt.ylabel("Y")
+   # plt.legend()
+
+
+            
 
 
     
